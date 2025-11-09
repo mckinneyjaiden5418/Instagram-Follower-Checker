bc3b552d8ac143443d6bfba2e7f1332143f18525
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-11-09 04:53:39 +0000

    All .json get ignored.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-"""Run script."""
+"""Run this to see who you're following that doesn't follow you back."""
 import json
+from typing import Any
 
 with open("followers_1.json", "r", encoding="utf-8") as f:
-    followers_data = json.load(f)
-followers = {user["string_list_data"][0]["value"] for user in followers_data}
+    followers_data: list[dict[str, Any]] = json.load(f)
+followers: set[str] = {user["string_list_data"][0]["value"] for user in followers_data}
 
 with open("following.json", "r", encoding="utf-8") as f:
-    following_data = json.load(f)
-following = {user["string_list_data"][0]["value"] for user in following_data["relationships_following"]}
+    following_data: dict[str, list[dict[str, Any]]] = json.load(f)
+following: set[str] = {user["title"] for user in following_data["relationships_following"]}
 
-not_following_back = following - followers
+not_following_back: set[str] = following - followers
 
 print(f"You follow {len(following)} people.")
 print(f"{len(not_following_back)} do not follow you back:")
